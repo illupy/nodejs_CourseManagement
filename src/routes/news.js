f652bcca1ca303f03f458e1767d24f28021bf737
illupy <tailuuduc2004@gmail.com>
2024-09-13 22:29:27 +0700

    clean code

--- src/routes/news.js
+++
@@ -1,7 +0,0 @@
-const newsController = require("../app/controllers/NewsController");
-const express=require('express')
-const router = express.Router();
-router.get("/:slug",newsController.show)
-router.get("/",newsController.index);
-
-module.exports = router;
